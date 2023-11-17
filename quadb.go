9a8b4c31fc8c3e8302201c8759068e850c503820
aaochieng <ochiengaoron@gmail.com>
2023-11-17 16:22:44 +0300

    Updated quadB

--- quadb.go
+++ quadb.go
@@ -1,51 +1,22 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "fmt"
 
 func QuadB(x, y int) {
 	if x > 0 && y > 0 {
-		// TopLine
-		z01.PrintRune('/')
-		for i := 0; i < x-2; i++ {
-			z01.PrintRune('*')
-		}
-		if x > 1 {
-			z01.PrintRune('\\')
-			z01.PrintRune('\n')
-		} else {
-			z01.PrintRune('\n')
-
-		}
-
-		// Middle lines
-		for i := 0; i < y-2; i++ {
-			z01.PrintRune('*')
-			for j := 0; j < x-2; j++ {
-				z01.PrintRune(' ')
-			}
-			if x > 1 {
-				z01.PrintRune('*')
-				z01.PrintRune('\n')
-			} else {
-				z01.PrintRune('\n')
-			}
-		}
-
-		// Bottom line
-		if y > 1 {
-			if x > 1 {
-				z01.PrintRune('\\')
-				for i := 0; i < x-2; i++ {
-					z01.PrintRune('*')
+		for i := 1; i <= y; i++ {
+			for j := 1; j <= x; j++ {
+				if (i == 1 && j == 1) || (i == y && j == x) {
+					fmt.Print("/")
+				} else if (i == y && j == 1) || (i == 1 && j == x) {
+					fmt.Print("\\")
+				} else if (j == 1) || (j == x) || (i == 1) || (i == y) {
+					fmt.Print("*")
+				} else {
+					fmt.Print(" ")
 				}
-				z01.PrintRune('/')
-				z01.PrintRune('\n')
-			} else {
-				z01.PrintRune('\\')
-				z01.PrintRune('\n')
 			}
+			fmt.Println()
 		}
 	}
 }
